a0672263df3c1250394656bf1cb18a5f45a97134
pospec4444 <2467395+pospec4444@users.noreply.github.com>
2020-10-16 18:17:35 +0100

    Updated test script

--- R/data-raw/test_duplicate_logic.R
+++ R/data-raw/test_duplicate_logic.R
@@ -8,18 +8,34 @@ library(readr)
 source("R/data-raw/functions.R")
 source("R/data-raw/maintenance/pcs_fixing.R")
 
+# Fix known problems in existing data
 p1 <- fix_pcs_results_women(pcs::rider_records_women)
 
 # are we good?
 test <- find_duplicate_results(p1)
 stopifnot(nrow(test) == 0)
 
-pcs_data <- get_pcs_data(c('hannah-barnes'))
-p2 <- pcs_data$results %>%
-  mutate(dup = "new data")
+# scrape women results
+rankings_url <- get_ranking_url("https://www.procyclingstats.com/rankings.php/we?cat=we")
+rider_urls <- get_rider_urls(rankings_url)
+pcs_data <- get_pcs_data(rider_urls)
 
-# 75th place
-subset(p1, date == '2016-10-15' &
-         rider == 'Hannah Barnes')
-# 76th place
-subset(p2, date == '2016-10-15')
+# Fix known problems in new data
+p2 <- fix_pcs_results_women(pcs_data$results)
+
+###
+## Actual "duplicate logic" code
+## p1: existing data (PCS errors fixed)
+## p2: latest data (PCS errors fixed)
+
+out <- full_join(p1, p2)
+dups <- find_duplicate_results(out)
+
+# remove every duplicate record from p1
+tp1 <- anti_join(p1, dups, by = c("rider", "date", "race", "stage"))
+# join with latest data
+out <- full_join(tp1, p2)
+
+# are we good?
+test <- find_duplicate_results(p1)
+stopifnot(nrow(test) == 0)
